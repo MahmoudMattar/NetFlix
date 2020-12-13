e68f0da7aaa09848c924104461247798f076da9d
Mohamed Reda <zredo100@gmail.com>
2020-12-14 01:30:45 +0200

    Revert "Creating: ( Auth listener - Browse component - Profiles component "Loading - Loading " )"
    This reverts commit 440e020163bbcd52e7356116ce0f66345846d71a.

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,24 +1,9 @@
-import React from 'react';
-import { BrowseContainer } from '../containers/browse';
-import { useContent } from '../hooks';
-import { selectionFilter } from '../utils';
+import React from "react";
 
 export default function Browse() {
-  //we need the series and the films
-    const { series } = useContent('series');
-    const { films } = useContent('films');
-    // console.log(series);
-    // console.log(films);
-
-    
-  //we need slides
-  const slides = selectionFilter({ series, films });
-    // console.log(slides);
-
-
-  //pass it ro browse container
-
-
-
-  return <BrowseContainer slides={slides} />;
+  return (
+    <>
+      <p>Hello from Browse</p>
+    </>
+  );
 }
