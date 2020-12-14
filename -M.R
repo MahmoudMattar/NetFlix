7c58962c1f48576503f26b179dd31386d9597e96
Mohamed Reda <zredo100@gmail.com>
2020-12-15 00:30:35 +0200

    delete MR

--- -M.R
+++
@@ -1,4 +0,0 @@
-M	src/components/index.js
-M	src/hooks/index.js
-M	src/pages/browse.js
-Branch 'Browse-and-Profiles-Component-' set up to track remote branch 'Browse-and-Profiles-Component-' from 'origin'.
