9b60fddc9ce90b1fc2148c9f73d615e311097300
Mohamed Reda <zredo100@gmail.com>
2020-12-15 00:27:09 +0200

    Browse and Profiles Component .. M.R

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React from 'react';
+import { BrowseContainer } from '../containers/browse';
+import { useContent } from '../hooks';
+import { selectionFilter } from '../utils';
 
 export default function Browse() {
-  return (
-    <>
-      <p>Hello from Browse</p>
-    </>
-  );
+
+  //we need the series and the films
+    const { series } = useContent('series');
+    const { films } = useContent('films');
+    // console.log(series);
+    // console.log(films);
+
+
+  //we need slides
+  const slides = selectionFilter({ series, films });
+    // console.log(slides);
+
+
+  //pass it ro browse container
+
+
+
+  return <BrowseContainer slides={slides} />;
 }
