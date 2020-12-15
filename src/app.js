ad63aacdd7462ac3591dc0705aff761853a8a7b3
MahmoudMattar <67396388+MahmoudMattar@users.noreply.github.com>
2020-12-15 17:39:06 +0200

    firebase config

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch } from "react-router-dom";
 import { Home, Browse, SignIn, SignUp } from "./pages";
 import * as ROUTES from "./constants/routes";
 import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
+
 import { useAuthListener } from "./hooks";
 
 export default function App() {
