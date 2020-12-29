b7d2f9d05ade7c04fd11625a7cea150d865414d7
MahmoudMattar <67396388+MahmoudMattar@users.noreply.github.com>
2020-12-29 21:34:13 +0200

    ok

--- Server/config/db.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  url: 'mongodb://127.0.0.1:27017/collections',
-  secret: 'secret',
-};
--- Server/routes/movies.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-router.get('/getUsersMovieList/:id', userController.returnUserMovieList);
-router.get('/testRoute', userController.testRouteController);
-router.put('/addToUserList/:id', userController.addToUserList);
-
-module.exports = router;
