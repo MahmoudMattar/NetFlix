253205c25c561018e0847fe8d80ca5c6c2aa3c2e
MahmoudMattar <67396388+MahmoudMattar@users.noreply.github.com>
2020-12-30 16:56:02 +0200

    fine

--- back-end-server/routes/movie.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const { returnUserMovieList, testRouteController, addToUserList } = require("../controllers/movie.controller");
-const authJwt = require("../middleware/authJwt");
-router.get("/getUsersMovieList/:id", authJwt.verifyToken, returnUserMovieList);
-router.get("/testRoute", authJwt.verifyToken, testRouteController);
-router.put("/addToUserList/:id", authJwt.verifyToken, addToUserList);
-
-module.exports = router;
